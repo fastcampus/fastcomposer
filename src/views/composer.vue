28d48f821d95a4ab61cb431b9622adf411dad8a6
Yoosoo Won <shelling104@gmail.com>
2020-02-18 11:01:31 +0900

    layouts: update composer guide(V2-108)

--- src/views/composer.vue
+++ src/views/composer.vue
@@ -43,70 +43,9 @@
         <button @click="onHideModal">
           <i class="material-icons">close</i>
         </button>
-        <table>
-          <thead>
-            <tr>
-              <th>태그명</th>
-              <th>표시</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>i</td>
-              <td><i>FastComposer</i></td>
-            </tr>
-            <tr>
-              <td>b</td>
-              <td><b>FastComposer</b></td>
-            </tr>
-            <tr>
-              <td>s</td>
-              <td><s>FastComposer</s></td>
-            </tr>
-            <tr>
-              <td>u</td>
-              <td><u>FastComposer</u></td>
-            </tr>
-            <tr>
-              <td>em</td>
-              <td><em>FastComposer</em></td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <thead>
-          <tr>
-            <th>단축키</th>
-            <th>기능</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>Alt+Shift+1</td>
-            <td>레이어 속성</td>
-          </tr>
-          <tr>
-            <td>Alt+Shift+2</td>
-            <td>미리보기</td>
-          </tr>
-          <tr>
-            <td>Alt+Shift+3</td>
-            <td>레이어 목록</td>
-          </tr>
-          <tr>
-            <td>Alt+Shift+0</td>
-            <td>레이아웃 목록</td>
-          </tr>
-          <tr>
-            <td>Up/Down/Home/End</td>
-            <td>이전/다음/처음/마지막 항목</td>
-          </tr>
-          <tr>
-            <td>Enter</td>
-            <td>선택 &amp; 편집</td>
-          </tr>
-          </tbody>
-        </table>
+        <div>
+          <img width="1024" height="996" src="https://storage.googleapis.com/static.fastcampus.co.kr/staging/uploads/202002/105732-39/composer-guide-2x.png" alt=""/>
+        </div>
       </div>
     </modal>
   </div>
